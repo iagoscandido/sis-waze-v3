a889b379bfa702d6d5da62556583d31a48ef6804
iago silva <iago.scandido3@gmail.com>
2025-08-18 15:47:26 -0300

    feat: add simple landing page with button to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-400 to-indigo-600">
+      <Link
+        href="/dashboard"
+        className="px-6 py-3 bg-white text-indigo-600 font-semibold rounded-lg shadow-md hover:bg-indigo-50 transition-colors duration-300"
+      >
+        Ir para o Dashboard
+      </Link>
+    </main>
+  );
 };
 
 export default page;
