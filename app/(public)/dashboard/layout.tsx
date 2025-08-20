f3088197185bae1c3161eb9db61c1ee7102d0f15
iago silva <iago.scandido3@gmail.com>
2025-08-19 23:13:35 -0300

    refact: to ES6 pattern

--- app/(public)/dashboard/layout.tsx
+++ app/(public)/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   return <main>{children}</main>;
 };
 
