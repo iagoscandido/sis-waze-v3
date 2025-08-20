9052ea8d3ba721893c8136ed6fd211d897cbf0c9
iago silva <iago.scandido3@gmail.com>
2025-08-19 23:13:50 -0300

    feat: add auth and autentication

--- app/(public)/dashboard/page.tsx
+++ app/(public)/dashboard/page.tsx
@@ -1,10 +1,21 @@
 import { DashboardLive } from "@/components/dashboard/DashboardLive";
+import { auth } from "@/lib/auth.server";
+import { redirect } from "next/navigation";
+import { Role } from "@prisma/client";
 
-export default function DashboardPage() {
+const DashboardPage = async () => {
+  const session = await auth();
+
+  if (!session || !session.user || session.user.role !== Role.ADMIN) {
+    redirect("/login");
+  }
+
   return (
     <main className="min-h-screen p-8">
       <h1 className="text-2xl font-bold mb-6">Rotas monitoradas</h1>
       <DashboardLive />
     </main>
   );
-}
+};
+
+export default DashboardPage;
