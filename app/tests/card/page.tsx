83b2fcc905b14d63d871ff4c6cc67eadae96d8c4
iago silva <iago.scandido3@gmail.com>
2025-09-04 14:28:37 -0300

    feat: add page test for new card

---
+++ app/tests/card/page.tsx
@@ -0,0 +1,9 @@
+import WazeIrregularities from "./waze-irregularities-card";
+
+export default function Page() {
+  return (
+    <main className="flex flex-wrap gap-1 items-center m-3">
+      <WazeIrregularities />
+    </main>
+  );
+}
